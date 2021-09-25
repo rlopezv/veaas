a400ce8323151a24adf6c09e3e3fd03d7aaa7686
rlopezv <ramon.viana@ezentis.com>
2021-09-25 21:02:38 +0200

    Including springboot application for editiong API specification

---
+++ app/src/main/java/com/github/rlopezv/veaas/VeaasApplication.java
@@ -0,0 +1,13 @@
+package com.github.rlopezv.veaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeaasApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/github/rlopezv/veaas/VeaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rlopezv.veaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
